41c0a692ae3b7f2e6e71e960f485bf6e4443160f
alineak <77595970+alineak@users.noreply.github.com>
2021-03-09 18:11:36 -0300

    Add files via upload

---
+++ dashcard/dash-cards/src/test/java/br/yoshimi/dashcards/DashCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.yoshimi.dashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
